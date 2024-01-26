819b66d0ba048573d1a4faf57ad532dc5ab7cef3
Tandra Malm <tandra_malm@hotmail.com>
2024-01-25 20:08:27 -0700

    updated comments in dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,13 +1,9 @@
-# Display a count of how many products are in the database
-# Display a count of how many categories are in the database
-
 class Admin::DashboardController < ApplicationController
-  # def index
-  #   @product_count = Product.count
-  # end
 
   def show
+    # Display a count of how many products are in the database
     @product_count = Product.count
+    # Display a count of how many categories are in the database
     @category_count = Category.count
   end
 end
