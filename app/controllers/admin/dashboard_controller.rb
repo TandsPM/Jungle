55d4fc4f89bf3cfe12593135d03d5e5d87f73d67
Tandra Malm <tandra_malm@hotmail.com>
2024-01-25 20:04:37 -0700

    added product count to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
+# Display a count of how many products are in the database
+# Display a count of how many categories are in the database
+
 class Admin::DashboardController < ApplicationController
+  # def index
+  #   @product_count = Product.count
+  # end
+
   def show
+    @product_count = Product.count
   end
 end
