e66197d281ea3b7a9ec2f6391e7aaaf0f6f01f0d
Tandra Malm <tandra_malm@hotmail.com>
2024-01-26 17:29:10 -0700

    added index, new, create to categories rb file

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,22 @@
+class Admin::CategoriesController < ApplicationController
+  # display list of all categories
+  def index
+    @category = Category.all
+  end
+
+  #display form for creating a new category
+  def new
+    @category = Category.new
+  end
+
+  # Handle the created of a new category
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+  end
+end
+
