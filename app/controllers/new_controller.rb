a84cb7434f7e33d85e68006fcf444e157dd5ca86
Tandra Malm <tandra_malm@hotmail.com>
2024-01-27 15:36:48 -0700

    added login/signup

---
+++ app/controllers/new_controller.rb
@@ -0,0 +1,11 @@
+class NewController < ApplicationController
+  
+  before_filter :authorize
+
+  def cool
+  end
+
+  def free
+  end
+
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    # If user exists AND password is correct
+    if user && user.authenticate(params[:password])
+      # Save user id inside browser cookie
+      # logged in when nav around website
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+end
